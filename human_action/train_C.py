9a7f31540315dd36c062d8d17af26d76e3d79b10
Mike D'Arcy <m.m.darcy@csuohio.edu>
2019-08-02 18:56:58 -0400

    Update DeepMoTIon trainer to paper version

--- human_action/train_C.py
+++ human_action/train_C.py
@@ -55,6 +55,114 @@ def build_train_data(angle_tol = 0):
 	#######################
 	max_velocity = 0.31
 	learning_rate = 0.1
+	data_file = 'data/training_human_data.json'
+	with open(data_file) as json_data:
+		data = json.load(json_data)
+	#bad_peds = [9, 10, 26, 52, 56, 115, 120, 129, 171, 212, 214, 216, 274, 277, 282, 284, 288, 290, 292, 295, 296, 297, 298, 334, 335, 339, 367]
+	#data = {k: v for k, v in data.items() if k not in bad_peds}
+	#print(np.array(data[list(data.keys())[0]]['radardata_list'][0]['observation']).shape)
+	#print(np.array(data[list(data.keys())[0]]['radardata_list'][0]['vel']))
+	#print(data.keys())
+	data_new        = {}
+	actions         = {}
+	cosined_actions = {}
+	targets         = {}
+	vel             = {}
+	
+	if angle_tol != 0:
+		confidence = list()
+		for i in range(0,180):
+			confidence.append(scipy.stats.norm(0,angle_tol).pdf(i))
+		for i in range(-180,0):
+			confidence.append(scipy.stats.norm(0,angle_tol).pdf(i))
+		confidence = np.reshape(np.array(confidence,dtype=np.float32),(1,360))
+		#confidence = np.roll(confidence,180)
+		#print (confidence)
+	else:
+		confidence = list()
+		confidence.append(1)
+		for i in range(0,359):
+			confidence.append(0)
+		confidence = np.reshape(np.array(confidence,dtype=np.float32),(1,360))
+		
+	#confidence = [0.325,0.6065,0.8825,1,0.8825,0.6065,0.325]
+	# confidence is calculated as the gaussian distribution
+	# centered in the middle with standard deviation 3
+	# This vector has been reweighted to give a value of 1 at the mean
+	for i in range(1):
+		## shifter is the angle to rotate the data with
+		## it is done in bins of
+		## each bin contains a random number in that bin
+		## the bins cover all 360 degrees.
+		
+		#shifter = 0 if i == 0  else random.randint(0,18) + (i-1)*18
+		shifter = 0 if i == 0  else random.randint(0,360)
+		for key in data.keys():
+			key_new = key + str(i)
+			data_new  [key_new] = []
+			actions   [key_new] = []
+			cosined_actions[key_new] = []
+			targets   [key_new] = []
+			vel       [key_new] = []
+			observations = np.array(data[key]['radardata_list'])
+			n = len(observations)
+			for i in range(len(observations)):
+				observation = np.array(observations[i]['observation'], dtype=np.float32)
+				observation = np.roll(observation,shifter) ##shifter
+				data_new[key_new].append(observation)
+				### compute action list ###
+				velocity = np.array(observations[i]['vel'], dtype=np.float32)
+				angle    = math.atan2(velocity[1],velocity[0])
+				angle    = math.degrees(angle)
+				angle    = (angle+360) % 360
+				action   = np.zeros((1,360),dtype=np.float32)
+				the_angle = int(math.floor(angle))
+				action[0,the_angle] = 1
+				cosined_action = np.roll(confidence,the_angle)
+				cosided_action = np.roll(cosined_action,shifter) ##shifter
+				action = np.roll(action,shifter) ##shifter
+				cosined_actions[key_new].append(cosined_action)
+				actions[key_new].append(action)
+			
+				veloc  = np.zeros((1,1))
+				velo   = np.sqrt(np.sum(np.power(velocity,2)))
+				velo   = velo if velo < max_velocity else max_velocity
+				veloc += velo/max_velocity
+				vel[key_new].append(veloc)
+			
+				### compute target list ###
+				target_position = np.zeros((1,2))
+				if i+target_dist+target_var < n:
+					for j in range(i+target_dist,i+target_dist+target_var):
+						target_position += np.array(observations[j]['position'])
+					target_position = target_position/target_var
+				else:
+					target_position += np.array(observations[n-1]['position'])
+				target_direction = target_position - np.array(observations[i]['position'])
+				target_angle    = math.atan2(target_direction[0,1],target_direction[0,0])
+				target_angle    =  math.degrees(target_angle)
+				target_angle    = (target_angle+360) % 360
+				target_action   = np.zeros((1,361),dtype=np.float32)
+				target_angle    = int(math.floor(target_angle))
+				target_action[0,target_angle] = 1
+				target_action[0,0:360] = np.roll(target_action[0,0:360],shifter) ##shifter
+				target_dist = math.sqrt(np.sum(np.power(target_direction,2)))
+				target_action[0,360] = target_dist
+				targets[key_new].append(target_action)
+	return data_new,targets,actions,cosined_actions,vel
+
+def build_complete_train_data(angle_tol = 0):
+	data_new        = {}
+	actions         = {}
+	cosined_actions = {}
+	targets         = {}
+	vel             = {}
+	#######################
+	target_dist = 30
+	target_var = 50000
+	#######################
+	max_velocity = 0.31
+	learning_rate = 0.1
 	data_file = 'data/complete_training_human_data.json'
 	with open(data_file) as json_data:
 		data = json.load(json_data)
@@ -148,7 +256,7 @@ def build_train_data(angle_tol = 0):
 				targets[key_new].append(target_action)
 	return data_new,targets,actions,cosined_actions,vel
 
-def build_test_data():
+def build_complete_test_data():
 	#######################
 	target_dist = 30
 	target_var = 50000
@@ -156,6 +264,77 @@ def build_test_data():
 	max_velocity = 0.31
 	learning_rate = 0.1
 	data_file = 'data/complete_testing_human_data.json'
+	#data_file = 'data/testing_human_data.json'
+	with open(data_file) as json_data:
+		data = json.load(json_data)
+	#print(np.array(data[list(data.keys())[0]]['radardata_list'][0]['observation']).shape)
+	#print(np.array(data[list(data.keys())[0]]['radardata_list'][0]['vel']))
+	#print(data.keys())
+	data_new        = {}
+	actions         = {}
+	targets         = {}
+	vel             = {}
+	# confidence is calculated as the gaussian distribution
+	# centered in the middle with standard deviation 3
+	# This vector has been reweighted to give a value of 1 at the mean
+
+	for key in data.keys():
+		observations = np.array(data[key]['radardata_list'])
+		n = len(observations)
+		if n<3:
+			continue;
+		data_new  [key] = []
+		actions   [key] = []
+		targets   [key] = []
+		vel       [key] = []
+		for i in range(len(observations)):
+			observation = np.array(observations[i]['observation'], dtype=np.float32)
+			data_new[key].append(observation)
+			### compute action list ###
+			velocity = np.array(observations[i]['vel'], dtype=np.float32)
+			angle    = math.atan2(velocity[1],velocity[0])
+			angle    = math.degrees(angle)
+			angle    = (angle+360) % 360
+			action   = np.zeros((1,360),dtype=np.float32)
+			the_angle = int(math.floor(angle))
+			action[0,the_angle] = 1
+			actions[key].append(action)
+		
+			veloc  = np.zeros((1,1))
+			velo   = np.sqrt(np.sum(np.power(velocity,2)))
+			velo   = velo if velo < max_velocity else max_velocity
+			veloc += velo/max_velocity
+			vel[key].append(veloc)
+		
+			### compute target list ###
+			target_position = np.zeros((1,2))
+			if i+target_dist+target_var < n:
+				for j in range(i+target_dist,i+target_dist+target_var):
+					target_position += np.array(observations[j]['position'])
+				target_position = target_position/target_var
+			else:
+				target_position += np.array(observations[n-1]['position'])
+			target_direction = target_position - np.array(observations[i]['position'])
+			target_angle    = math.atan2(target_direction[0,1],target_direction[0,0])
+			target_angle    =  math.degrees(target_angle)
+			target_angle    = (target_angle+360) % 360
+			target_action   = np.zeros((1,361),dtype=np.float32)
+			target_angle    = int(math.floor(target_angle))
+			target_action[0,target_angle] = 1
+			target_dist = math.sqrt(np.sum(np.power(target_direction,2)))
+			target_action[0,360] = target_dist
+			targets[key].append(target_action)
+	return data_new,targets,actions,vel
+
+
+def build_test_data():
+	#######################
+	target_dist = 30
+	target_var = 50000
+	#######################
+	max_velocity = 0.31
+	learning_rate = 0.1
+	data_file = 'data/testing_human_data.json'
 	with open(data_file) as json_data:
 		data = json.load(json_data)
 	#print(np.array(data[list(data.keys())[0]]['radardata_list'][0]['observation']).shape)
@@ -218,7 +397,7 @@ def build_test_data():
 	return data_new,targets,actions,vel
 
 #network_list = ['action+','action','feature','GRP','GRP+','GRP_feature']
-def train(file_name, data_new,targets,actions,cosined_actions,vel):
+def train(file_name, data_new,targets,actions,cosined_actions,vel,dist=0.2):
 	#######################
 	target_dist = 30
 	target_var = 50000
@@ -226,7 +405,11 @@ def train(file_name, data_new,targets,actions,cosined_actions,vel):
 	max_velocity = 0.31
 	learning_rate = 0.1
 	if network == 'GRP':
-		f1 = GRP((2,360),(1,361),(1,360),(1,1),False,False,max_velocity,file_name=file_name)
+		f1 = GRP((2,360),(1,361),(1,360),(1,1),False,False,max_velocity,file_name=file_name, safe_dist=dist)
+		f1.train_network(data_new,targets,cosined_actions,vel,actions)
+	elif network == 'action':
+		f1 = action_predicter((2,360),(1,361),(1,360),(1,1),False,False,max_velocity,file_name=file_name, safe_dist=dist)
+		#print(len(data_new), sum(len(data_new[k]) for k in data_new))
 		f1.train_network(data_new,targets,cosined_actions,vel,actions)
 	elif network == 'action_m':
 		f1 = action_modified((2,360),(1,361),(1,360),(1,1),False,False,max_velocity)
@@ -244,6 +427,9 @@ def test(file_name,data_new,targets,actions,vel):
 	if network == 'GRP':
 		f1 = GRP((2,360),(1,361),(1,360),(1,1),True,True,max_velocity,file_name = file_name)
 		return f1.test_network(data_new,targets,actions,vel,True)
+	elif network == 'action':
+		f1 = action_predicter((2,360),(1,361),(1,360),(1,1),True,True,max_velocity,file_name = file_name)
+		return f1.test_network(data_new,targets,actions,vel,True)
 	elif network == 'action_m':
 		f1 = action_modified((2,360),(1,361),(1,360),(1,1),True,True,max_velocity)
 		return f1.test_network(data_new,targets,actions,vel,True)
@@ -258,31 +444,72 @@ def main():
 		data_new,targets,actions,vel = build_test_data()
 		cl_error,an_error,v_error = test('dnns/action_m'+str(std)+'.dnn',data_new,targets,actions,vel)
 		sys.exit(1)
-	stds = [1,0,2,3,4,5,6,7,8,9,10,15,20,25,30,40,50,60,70,80,90]
-	#stds = [1,2,3,4,5,6,7,8,9,10,15,20,25,30,40,50,60,70,80,90]
-	for std in stds:
-		if std !=1:
-			data_new,targets,actions,cosined_actions,vel = build_train_data(std)
-			train('dnns/GRP_'+str(std)+'.dnn', data_new,targets,actions,cosined_actions,vel)
-		data_new,targets,actions,vel = build_test_data()
-		cl_error,an_error,v_error = test('dnns/GRP_'+str(std)+'.dnn',data_new,targets,actions,vel)
-		if os.path.isfile('recording.json'):
-			with open ('recording.json') as recording_data:
-				recording = json.load(recording_data)
-		else:
-			recording = []
-		this_recording = {}
-		this_recording['name'] = 'GRP'
-		this_recording['std'] = std
-		this_recording['classification_error'] = cl_error
-		this_recording['angular_error'] = an_error
-		this_recording['velocity_error'] = v_error
-
-		recording.append(this_recording)
+	elif network == 'action':
 		
-		with open('recording.json','w') as recording_data:
-			json.dump(recording, recording_data, indent=4, sort_keys=True)
+		stds = [0, 5, 10, 15, 20, 25]
+		distances = [0.35, 0.35, 0.35, 0.35, 0.35, 0.35]
+		#stds = [5]
+		#distances = [0.35]
+		for std,dist in zip(stds,distances):
+			print('Building train data (may take a while)...')
+			data_new,targets,actions,cosined_actions,vel = build_complete_train_data(std)
+			#data_new,targets,actions,cosined_actions,vel = build_train_data(std)
+			print('Done building train data.')
 
+			train('dnns/action_'+str(std)+'_'+str(dist)+'.dnn', data_new,targets,actions,cosined_actions,vel, dist)
+
+			print('Building test data (may take a while)...')
+			data_new,targets,actions,vel = build_complete_test_data()
+			#data_new,targets,actions,vel = build_test_data()
+			print('Done building test data.')
+
+			cl_error,an_error,v_error = test('dnns/action_'+str(std)+'_'+str(dist)+'.dnn',data_new,targets,actions,vel)
+
+			if os.path.isfile('recording.json'):
+				with open ('recording.json') as recording_data:
+					recording = json.load(recording_data)
+			else:
+				recording = []
+			this_recording = {}
+			this_recording['name'] = 'action'
+			this_recording['std'] = std
+			this_recording['classification_error'] = cl_error
+			this_recording['angular_error'] = an_error
+			this_recording['velocity_error'] = v_error
+
+			recording.append(this_recording)
+			
+			with open('recording.json','w') as recording_data:
+				json.dump(recording, recording_data, indent=4, sort_keys=True)
+	elif network == 'GRP':
+		#stds = [0,1,2,3,4,5,6,7,8,9,10,15,20,25,30,40,50,60,70,80,90]
+		stds = [5,5,5]
+		distances = [0.8,1,1.5]
+		for std,dist in zip(stds,distances):
+			if dist != 0.35:
+				data_new,targets,actions,cosined_actions,vel = build_complete_train_data(std)
+				train('dnns/GRP_'+str(std)+'_'+str(dist)+'.dnn', data_new,targets,actions,cosined_actions,vel,dist)
+			data_new,targets,actions,vel = build_complete_test_data()
+			cl_error,an_error,v_error = test('dnns/GRP_'+str(std)+'_'+str(dist)+'.dnn',data_new,targets,actions,vel)
+			if os.path.isfile('recording.json'):
+				with open ('recording.json') as recording_data:
+					recording = json.load(recording_data)
+			else:
+				recording = []
+			this_recording = {}
+			this_recording['name'] = 'GRP'
+			this_recording['std'] = std
+			this_recording['classification_error'] = cl_error
+			this_recording['angular_error'] = an_error
+			this_recording['velocity_error'] = v_error
+
+			recording.append(this_recording)
+			
+			with open('recording.json','w') as recording_data:
+				json.dump(recording, recording_data, indent=4, sort_keys=True)
+	else:
+		raise ValueError("Error: Unknown network: {}".format(network))
+		sys.exit(1)
 if __name__ == '__main__':
 	main()
 
